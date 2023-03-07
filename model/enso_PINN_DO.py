e177bdd0535e926f5f4bdad3fd1cf54fc0116442
flyerecnu <43198313+flyerecnu@users.noreply.github.com>
2023-03-07 16:16:21 +0800

    Add files via upload

---
+++ model/enso_PINN_DO.py
@@ -0,0 +1,164 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import pandas as pd
+from scipy.io import loadmat
+import numpy as np
+
+# data load
+total_data = np.load(r'/data/home/scv6730/run/new_code/total_data.npy')
+
+# device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+input_tensor = total_data
+# print('input_tensor', input_tensor.shape)
+
+
+# 时间步data构造函数
+def creat_dataset(dataset, look_back):
+    data_x = []
+    data_y = []
+    for i in range(len(dataset) - look_back*2):
+        data_x.append(dataset[i:i+look_back])
+        # data_y.append(dataset[i+look_back])
+        data_y.append(dataset[i+look_back:i+2*look_back])    # 输入dim = 输出dim
+    return np.asarray(data_x), np.asarray(data_y)
+
+
+# Neural Network
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(5, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 50),
+            torch.nn.Tanh(),
+            torch.nn.Linear(50, 4)
+        )
+
+    def forward(self, x):
+        # print('x.shape:', x.shape)
+        a, b, c = x.shape    # batch, timesteps, indim
+        # x = x.view(-1, c)
+        out = self.net(x.view(-1, c))
+        out = out.view(a, b, -1)
+        # print('out.shape', out.shape)
+
+        return out
+
+# train
+def train(model, criterion, optimizer, train_data):
+    model.train()
+    epoch_loss = 0
+    for i, batch in enumerate(train_data):
+        src, tgt = batch
+        # print('src_t.size0:', src.shape)
+        src = src.to(device)
+        tgt = tgt.to(device)
+
+        src_t = src[:, :, 0].requires_grad_()
+        src_SSTA= src[:, :, 1].requires_grad_()
+        src_hA= src[:, :, 2].requires_grad_()
+        src_TAO1= src[:, :, 3].requires_grad_()
+        src_TAO2= src[:, :, 4].requires_grad_()
+        src = torch.stack((src_t, src_SSTA, src_hA, src_TAO1, src_TAO2), dim=0)
+        # print('src_t.size1:', src.shape)
+
+        src = src.permute(1, 2, 0).requires_grad_()
+        # print('src_t.size1:', src.shape)
+
+        with torch.backends.cudnn.flags(enabled=False):
+            out = model(src)
+        # print('out', out.shape)
+
+        SSTA_pred = out[:, :, 0].requires_grad_()
+        HA_pred = out[:, :, 1].requires_grad_()
+        TAO1_pred = out[:, :, 2].requires_grad_()
+        TAO2_pred = out[:, :, 3].requires_grad_()
+
+        tgt_SSTA = tgt[:, :, 1]
+        tgt_HA = tgt[:, :, 2]
+        tgt_TAO1 = tgt[:, :, 3]
+        tgt_TAO2 = tgt[:, :, 4]
+
+        T_t = torch.autograd.grad(SSTA_pred, src_t, grad_outputs=torch.ones_like(SSTA_pred), retain_graph=True, create_graph=True)[0]
+        tao1_t = torch.autograd.grad(TAO1_pred, src_t, grad_outputs=torch.ones_like(TAO1_pred), retain_graph=True, create_graph=True)[0]
+
+        f_2 = T_t - 150*TAO1_pred + 250*TAO1_pred*(src_t - 5/12) + 1.2 * (SSTA_pred ** 3)
+        g_2 = tao1_t - 0.036*SSTA_pred + 2*TAO1_pred
+
+        # f_2 = 150*TAO1_pred - 250*TAO1_pred*(src_t - 5/12) - 1.2 * (SSTA_pred ** 3)
+        # g_2 = 0.036*SSTA_pred - 2*TAO1_pred
+
+        loss1 = criterion(SSTA_pred, tgt_SSTA) + criterion(HA_pred, tgt_HA) + criterion(TAO1_pred, tgt_TAO1) + criterion(TAO2_pred, tgt_TAO2)
+        # loss1 = criterion(SSTA_pred, tgt_SSTA) + criterion(TAO1_pred, tgt_TAO1)
+        loss2 = torch.mean(torch.pow((f_2), 2)) + torch.mean(torch.pow((g_2), 2))
+        lam = 10
+        loss = lam * loss1 + loss2
+        loss = loss.to(device)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+    return epoch_loss / len(train_data)
+
+def main():
+    look_back = 12
+
+    batch_size = 64
+    epochs = 20000
+    epoch_loss_all = []
+    best_loss = 300
+
+    input_tensor = total_data
+    print('input_tensor', input_tensor.shape)
+
+    dataX, dataY = creat_dataset(input_tensor, look_back=look_back)
+    print('dataX', dataX.shape)
+    train_size = int(len(dataX) * 0.9)
+    # train_size = int(len(dataX))
+
+    # train set
+    train_x = dataX[:train_size]
+    print('train_x', train_x.shape)
+    train_y = dataY[:train_size]
+    print('train_y', train_y.shape)
+
+    train_x = torch.from_numpy(train_x.astype(np.float32))
+    train_y = torch.from_numpy(train_y.astype(np.float32))
+
+    train_data =torch.utils.data.TensorDataset(train_x, train_y)
+    train_loader = DataLoader(train_data, batch_size=batch_size, num_workers=1)
+
+    U = MLP()
+    U = U.to(device)
+
+    optimizer = torch.optim.Adam(U.parameters(), lr=0.003)
+    criterion = nn.MSELoss()
+    for i in range(epochs):
+        epoch_loss = train(U, criterion, optimizer, train_loader)
+        epoch_loss_all.append(epoch_loss)
+        print("epoch: {} PINN2_12_2_train loss: {}".format(i, epoch_loss))
+        if epoch_loss < best_loss:
+            # best_loss = epoch_loss
+            model_name = r'/data/home/scv6730/run/model/NN/2/PINN2_12_2_model_{0:.5f}.pt'.format(epoch_loss)
+            torch.save(U.state_dict(), model_name)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ model/enso_PIlstm_DO.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import pandas as pd
+from scipy.io import loadmat
+import numpy as np
+
+# data load
+total_data = np.load(r'/data/home/scv6730/run/new_code/total_data.npy')
+
+# device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def creat_dataset(dataset, look_back):
+    data_x = []
+    data_y = []
+    for i in range(len(dataset) - look_back*2):
+        data_x.append(dataset[i:i+look_back])
+        # data_y.append(dataset[i+look_back])
+        data_y.append(dataset[i+look_back:i+2*look_back])    # 输入dim = 输出dim
+    return np.asarray(data_x), np.asarray(data_y)
+
+
+# LSTM_model
+class Lstm_enso(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(Lstm_enso, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size   # output_size = 1
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=0.5)
+        self.fc0 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc1 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc3 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc4 = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, x):
+        x1, _ = self.lstm(x)
+        a, b, c = x1.shape
+        # print('a,b,c:', a, b, c)    # (Batch, look_back, hidden_size)
+        # print('x1', x1.view(-1, c).shape)
+        out0, out1, out2, out3, out4 = self.fc0(x1.view(-1, c)), self.fc1(x1.view(-1, c)), self.fc2(x1.view(-1, c)), self.fc3(x1.view(-1, c)), self.fc4(x1.view(-1, c))
+
+        # print('out1', out1.shape)
+        out0, out1, out2, out3, out4 = out0.view(a, b, -1), out1.view(a, b, -1), out2.view(a, b, -1), out3.view(a, b, -1), out4.view(a, b, -1)
+        # print('out1', out1.shape)   # (Batch, look_back, out_dim)
+        out = torch.stack((out0, out1, out2, out3, out4), dim=0)
+        # print('out', out.shape)
+        out = out.permute(3, 1, 2, 0).squeeze()
+        # print('out', out.shape)
+
+        return out
+
+
+# train
+def train(model, criterion, optimizer, train_data):
+    model.train()
+    epoch_loss = 0
+    for i, batch in enumerate(train_data):
+        src, tgt = batch
+        # print('src_t.size0:', src.shape)
+        src = src.to(device)
+        tgt = tgt.to(device)
+
+        src_t = src[:, :, 0].requires_grad_()
+        src_SSTA = src[:, :, 1].requires_grad_()
+        src_hA = src[:, :, 2].requires_grad_()
+        src_TAO1 = src[:, :, 3].requires_grad_()
+        src_TAO2 = src[:, :, 4].requires_grad_()
+        src = torch.stack((src_t, src_SSTA, src_hA, src_TAO1, src_TAO2), dim=0)
+        # print('src_t.size1:', src.shape)
+
+        src = src.permute(1, 2, 0).requires_grad_()
+        # print('src_t.size1:', src.shape)
+
+        with torch.backends.cudnn.flags(enabled=False):
+            out = model(src)
+        # print('out', out.shape)
+
+        SSTA_pred = out[:, :, 1].requires_grad_()
+        HA_pred = out[:, :, 2].requires_grad_()
+        TAO1_pred = out[:, :, 3].requires_grad_()
+        TAO2_pred = out[:, :, 4].requires_grad_()
+
+        tgt_SSTA = tgt[:, :, 1]
+        tgt_HA = tgt[:, :, 2]
+        tgt_TAO1 = tgt[:, :, 3]
+        tgt_TAO2 = tgt[:, :, 4]
+
+        T_t = torch.autograd.grad(SSTA_pred, src_t, grad_outputs=torch.ones_like(SSTA_pred), retain_graph=True, create_graph=True)[0]
+        tao1_t = torch.autograd.grad(TAO1_pred, src_t, grad_outputs=torch.ones_like(TAO1_pred), retain_graph=True, create_graph=True)[0]
+
+        f_2 = T_t - 150*TAO1_pred + 250*TAO1_pred*(src_t - 5/12) + 1.2 * (SSTA_pred ** 3)
+        g_2 = tao1_t - 0.036*SSTA_pred + 2*TAO1_pred
+
+        # f_2 = 150 * TAO1_pred - 250 * TAO1_pred * (src_t - 5 / 12) - 1.2 * (SSTA_pred ** 3)
+        # g_2 = 0.036 * SSTA_pred - 2 * TAO1_pred
+
+        loss1 = criterion(SSTA_pred, tgt_SSTA) + criterion(HA_pred, tgt_HA) + criterion(TAO1_pred, tgt_TAO1) + criterion(TAO2_pred, tgt_TAO2)
+        # loss1 = criterion(SSTA_pred, tgt_SSTA) + criterion(TAO1_pred, tgt_TAO1)
+        loss2 = torch.mean(torch.pow((f_2), 2)) + torch.mean(torch.pow((g_2), 2))
+        # loss3 = criterion(T_t, f_2) + criterion(tao1_t, g_2)
+
+        lam = 10
+
+        loss = loss2 + lam * loss1
+        loss = loss.to(device)
+
+        # loss = criterion(out, tgt)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+    return epoch_loss / len(train_data)
+
+def main():
+    look_back = 6
+
+    input_size = 5
+    hidden_size = 10
+    num_layers = 2
+    output_size = 1
+
+    batch_size = 32
+    epochs = 2000
+    epoch_loss_all = []
+    best_loss = 500
+
+    input_tensor = total_data
+    print('input_tensor', input_tensor.shape)
+
+    dataX, dataY = creat_dataset(input_tensor, look_back=look_back)
+    print('dataX', dataX.shape)
+    train_size = int(len(dataX) * 0.9)
+    # train_size = int(len(dataX))
+
+    # train set
+    train_x = dataX[:train_size]
+    print('train_x', train_x.shape)
+    train_y = dataY[:train_size]
+    print('train_y', train_y.shape)
+
+    # train_x = train_x.reshape(-1, look_back, input_size)
+    # print('train_x', train_x.shape)
+    # train_y = train_y.reshape(-1, 1, input_size)
+    # print('train_y', train_y.shape)
+
+
+    train_x = torch.from_numpy(train_x.astype(np.float32))
+    train_y = torch.from_numpy(train_y.astype(np.float32))
+
+    train_data =torch.utils.data.TensorDataset(train_x, train_y)
+    train_loader = DataLoader(train_data, batch_size=batch_size, num_workers=1)
+
+    enso_pred = Lstm_enso(input_size, hidden_size, num_layers, output_size)
+    enso_pred = enso_pred.to(device)
+
+    optimizer = torch.optim.Adam(enso_pred.parameters(), lr=0.003)
+    criterion = nn.MSELoss()
+    # criterion = nn.L1Loss()
+
+    for i in range(epochs):
+        epoch_loss = train(enso_pred, criterion, optimizer, train_loader)
+        epoch_loss_all.append(epoch_loss)
+        print("epoch: {} PILSTM2_1_20_train loss: {}".format(i, epoch_loss))
+        if epoch_loss < best_loss:
+            # best_loss = epoch_loss
+            model_name = r'/data/home/scv6730/run/model/LSTM/2/PILSTM2_1_20_model_{0:.5f}.pt'.format(epoch_loss)
+            torch.save(Wind_pred.state_dict(), model_name)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
